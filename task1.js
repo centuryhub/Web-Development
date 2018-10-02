23bb5bd48950a6be0d807e550dbe431e6e800162
A. A. Malik <43787647+centuryhub@users.noreply.github.com>
2018-10-02 09:11:01 -0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,36 @@
+            function fun1(){
+                document.getElementById("box").value=document.getElementById("box").value + " 1"
+            }
+            function fun2(){
+                document.getElementById("box").value=document.getElementById("box").value + "2";
+            }
+
+            function fun3(){
+                document.getElementById("box").value=document.getElementById("box").value + "3";
+            }
+
+            function fun4(){
+                document.getElementById("box").value=document.getElementById("box").value + "4";
+            }
+            function fun5(){
+                document.getElementById("box").value=document.getElementById("box").value + "5";
+            }
+            function fun6(){
+                document.getElementById("box").value=document.getElementById("box").value + "6";
+            }
+            function fun7(){
+                document.getElementById("box").value=document.getElementById("box").value + "7";
+            }
+            function fun8(){
+                document.getElementById("box").value=document.getElementById("box").value + "8";
+            }
+            function fun9(){
+                document.getElementById("box").value=document.getElementById("box").value + "9";
+            }
+            function fun0(){
+                document.getElementById("box").value=document.getElementById("box").value + "0";
+            }
+
+            function funClear(){
+                document.getElementById("box").value="";
+            }
